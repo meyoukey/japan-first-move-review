1d5f306c03a6eb51523c50546e925c554059f111
meyoukey <meyoukey@users.noreply.github.com>
2026-07-18 18:36:31 +0900

    Fix custom food card redirect loop

--- functions/food-card/custom.js
+++ functions/food-card/custom.js
@@ -1,5 +1,9 @@
-export function onRequestGet({ request }) {
+export function onRequestGet({ request, next }) {
   const url = new URL(request.url);
+  if (url.pathname !== "/food-card/custom") {
+    return next();
+  }
+
   url.pathname = "/food-card/custom/";
   return Response.redirect(url.toString(), 301);
 }
