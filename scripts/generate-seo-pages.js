48e9b90837def7707cb9643b2120c7aa9ca46ec4
meyoukey <meyoukey@users.noreply.github.com>
2026-07-26 14:10:33 +0900

    fix image preview scrolling on desktop

--- scripts/generate-seo-pages.js
+++ scripts/generate-seo-pages.js
@@ -285,7 +285,7 @@ function headMarkup(page) {
       href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@12..96,400..800&display=swap"
       rel="stylesheet"
     />
-    <link rel="stylesheet" href="/styles.css?v=css-modules-18" />
+    <link rel="stylesheet" href="/styles.css?v=css-modules-19" />
   </head>`;
 }
 
